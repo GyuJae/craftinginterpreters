7d2ea6f9a4efa3fe6c5f4578d3a2c64ffedbae0f
정규재 <wjdrbwo1206@naver.com>
2025-07-30 17:33:00 +0900

    fix: eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,44 @@
+// eslint.config.js (ESM)
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
 
-
-
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import("eslint").Linter.FlatConfig[]} */
 export default [
-  {files: ["src/**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.node }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  eslintPluginUnicorn.configs.all,
+    {
+        files: ["src/**/*.{js,ts}"],
+        ignores: [
+            "**/node_modules/**",
+            "**/.git/**",
+            "**/build/**",
+            "**/*.lox",
+            "**/*.c",
+            "**/*.h",
+            "**/*.java",
+            "**/*.mjs",
+            "**/*.md",
+            "**/*.json",
+            "**/clox/**",
+            "**/lox/**",
+            "**/test/**",
+        ],
+        languageOptions: {
+            parser: tseslint.parser,
+            parserOptions: {
+                ecmaVersion: "latest",
+                sourceType: "module",
+            },
+            globals: globals.node,
+        },
+        plugins: {
+            "@typescript-eslint": tseslint.plugin,
+            unicorn: eslintPluginUnicorn,
+        },
+        rules: {
+            ...pluginJs.configs.recommended.rules,
+            ...tseslint.configs.recommended[0].rules,
+            ...eslintPluginUnicorn.configs.recommended.rules,
+        },
+    },
 ];
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-console.log("Hello via Bun!");
-
-export class A {
-    Add(a: number, b: number): number {
-        const result = a + b;
-        return result;
-    }
-}
